fb09cc378b82f19106e6dbb5f42b700eb8017635
Ikezogwo Kosisochukwu <ikezogwokosiso16@gmail.com>
2024-07-05 23:16:11 +0100

    added a return to top event and function

--- app/page.js
+++ app/page.js
@@ -24,7 +24,20 @@ export default function Home() {
       top.classList.toggle('returnToTop', window.scrollY > 0)
     })
     
+
+    
   })
+
+  const handleClick = ()=>{
+    return(
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+        })
+    )
+  }
+
+
   
   
 
@@ -32,7 +45,7 @@ export default function Home() {
     <main className=' bg-gray-300 px-10 min-h-screen'>
 
       <div className="top">
-        <button>
+        <button onClick={handleClick}>
           <svg className="return-logo" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v13m0-13 4 4m-4-4-4 4"/>
           </svg>
