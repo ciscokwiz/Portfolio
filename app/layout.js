04be7a8906f9d22115adb9d256ea8127950ecbdb
Ikezogwo Kosisochukwu <ikezogwokosiso16@gmail.com>
2024-07-22 18:44:45 +0100

    imported speed insight module

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         
         {children}
         <Analytics/>
+        <SpeedInsights />
       </body>
     </html>
   );
