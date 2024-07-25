30b285bc55258492de3fbe5823067ed85f711fe3
Ikezogwo Kosisochukwu <ikezogwokosiso16@gmail.com>
2024-07-25 04:03:20 +0100

    minor updates

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -23,10 +23,10 @@ export default function Navbar(){
     return(
         <div className='flex justify-between nav'>
             <div>
-                <a className='text-l mr-1 text-blue-900 cursor-default font-bold' href='/'>My Portfolio</a>
+                <a className='text-l mr-1 text-blue-900 cursor-default font-bold logo-text' href='/'>My Portfolio</a>
             </div>
             <div>
-                <ul className=' flex justify-evenly gap-3'>
+                <ul className=' flex justify-evenly gap-3 nav-icon'>
                     <li><button onClick={darkMode}><BsFillMoonStarsFill className=' fill-black cursor-pointer mt-1 mode'/></button></li>
                     <li>
                         <Link className=' bg-cyan-500 px-1 py-1 text-white rounded-md ml-5 hover:bg-slate-400 cursor-pointer resume' href="https://docs.google.com/document/d/182mmhX3ae6CSwpFyhrKKbbPj-YjeIN_trmnBnB_HB_Y/edit?usp=sharing">Resume</Link>
